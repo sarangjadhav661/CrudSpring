03f8d92c713c5997ebffb98376fc03971e012324
sarang <sarangjadhav661@gmail.com>
2024-05-01 19:34:46 +0530

    commit

--- src/main/java/com/api/Entity/Student.java
+++ src/main/java/com/api/Entity/Student.java
@@ -2,20 +2,23 @@ package com.api.Entity;
 
 import org.hibernate.internal.build.AllowSysOut;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+
 public class Student {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private int id;
 	
-	
+    @Column(nullable = false)
 	private String name;
+    @Column(nullable = false)
 	private String city;
 	
 	
